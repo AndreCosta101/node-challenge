e078834719844c564d24746d30521ac8d10c6af9
AndreCosta <andre.costa101@gmail.com>
2022-09-14 14:34:58 -0300

    bug correction

---
+++ README.md
@@ -0,0 +1,2 @@
+Find the docs under http://localhost:3001/api-docs/#/
+
--- src/controllers/users/CreateUserController.ts
+++ src/controllers/users/CreateUserController.ts
@@ -8,6 +8,10 @@ class CreateUserController {
 
             if (!name || !email || !password) return response.status(400).send({message: 'Name, email and password are mandatory'});
 
+            const userAlreadyExists = await User.findOne({email});
+
+            if(userAlreadyExists) return response.status(400).send({message: 'User already exists'});
+
             const user = await User.create({
                 name, 
                 email, 
@@ -15,8 +19,8 @@ class CreateUserController {
             })
 
             if(!user) return response.status(400).send({message: 'Error creating User'})
-            user.password = '';
-            return response.status(200).json(user)
+            user.password = '[SECRET]';
+            return response.status(201).json(user)
             
         } catch (error) {
             return response.status(500).send({ message: error.message})
